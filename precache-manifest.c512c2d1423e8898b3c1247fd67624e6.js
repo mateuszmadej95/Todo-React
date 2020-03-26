e8575f79fa85bc1abd56bdabbd490e86980bf604
Mateusz Madej <madej.mateusz.95@gmail.com>
2020-03-26 22:46:13 +0100

    first commit

---
+++ precache-manifest.c512c2d1423e8898b3c1247fd67624e6.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4b75cffcbe9c5e3ab77505eea781b090",
+    "url": "/index.html"
+  },
+  {
+    "revision": "328781071822d482a8e5",
+    "url": "/static/css/main.900aa292.chunk.css"
+  },
+  {
+    "revision": "c75008d5305202d078fa",
+    "url": "/static/js/2.ac046793.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.ac046793.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "328781071822d482a8e5",
+    "url": "/static/js/main.eadc1ec3.chunk.js"
+  },
+  {
+    "revision": "1e97a236ea4d5de4bc84",
+    "url": "/static/js/runtime-main.b64dda36.js"
+  }
+]);
